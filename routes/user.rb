efe570548c947b1823f90e95700bd1e93f5237bf
Carla Ares <carla.ares@honou.com.ar>
2014-03-14 10:36:28 -0300

    adding d3 and rickshaw js libraries

--- routes/user.rb
+++ routes/user.rb
@@ -7,7 +7,20 @@ class BusinessMetrics < Sinatra::Base
     erb :index
   end
 
-  get '/graphs' do
+  get '/graphs/:resource' do
+    database_name = params[:resource]
+    collection_name = 'counters.production.facebook.login_10'
+    today_unix = Date.today.to_time.to_i
+    start_week_unix = (Date.today - 7).to_time.to_i
+    start_month_unix = (Date.today - 30).to_time.to_i
+
+    @today_values = settings.mongo_connection.db(database_name)[collection_name].find( { :time => { :$gte => start_week_unix } } ).to_a
+    #@week_values = settings.mongo_connection.db(database_name)[collection_name].find( { :time => { :$gte => start_week_unix } } ).to_a
+    #@month_values = settings.mongo_connection.db(database_name)[collection_name].find( { :time => { :$gte => start_month_unix } } ).to_a
+
+    collection_name = 'counters.production.publications.search_10'
+    @publications_today_values = settings.mongo_connection.db(database_name)[collection_name].find( { :time => { :$gte => start_week_unix } } ).to_a
+
     erb :graphs
   end
 
